f245fa71e174768142a1a4247e6b98fab20c17f2
tuyentran <vat1906@gmail.com>
2021-08-15 19:34:20 +0700

    Update

--- 207_course_schedule/day1/solution.go
+++ 207_course_schedule/day1/solution.go
@@ -3,13 +3,14 @@ package day1
 func canFinish(numCourses int, prerequisites [][]int) bool {
 	visited := make([]bool, numCourses)
 	graph := make([][]int, numCourses)
+	dp := make([]bool, numCourses)
 
 	for i := 0; i < len(prerequisites); i++ {
 		graph[prerequisites[i][1]] = append(graph[prerequisites[i][1]], prerequisites[i][0])
 	}
 
 	for i := 0; i < numCourses; i++ {
-		if !dfs(graph, visited, i) {
+		if !dfs(graph, visited, dp, i) {
 			return false
 		}
 	}
@@ -17,18 +18,19 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	return true
 }
 
-func dfs(graph [][]int, visited []bool, course int) bool {
+func dfs(graph [][]int, visited []bool, dp []bool, course int) bool {
 	if visited[course] {
-		return false
+		return dp[course]
 	}
 
 	visited[course] = true
 	for i := 0; i < len(graph[course]); i++ {
-		if !dfs(graph, visited, graph[course][i]) {
+		if !dfs(graph, visited, dp, graph[course][i]) {
+			dp[course] = false
 			return false
 		}
 	}
 
-	visited[course] = false
+	dp[course] = true
 	return true
 }
